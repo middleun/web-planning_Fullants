f93c17318c7a746dce19b6e4b83083d649272ef5
middleun <eunallaco@gmail.com>
2021-03-30 18:45:47 +0900

    210330_slide section 작업중

--- js/slider.js
+++ js/slider.js
@@ -6,11 +6,11 @@ $(function(){
     let itemWidth=0;
     let viewSlide=0;
     let firstItem=$(".slide-item:first-child");
-    let lastItem=$(".slide-item:last-child");
+    // let lastItem=$(".slide-item:last-child");
     let cloneFirst=$(firstItem).clone();
-    let cloneLast=$(lastItem).clone();
-    // console.log(firstItem);
-
+    // let cloneLast=$(lastItem).clone();
+    console.log(firstItem,lastItem);
+// 클론하고 지우고 뒤에 붙이고
     
     itemLength=$(".slide-item").length;
     $(".slide-box").width($(".slide-item").outerWidth()*itemLength);
@@ -22,7 +22,7 @@ $(function(){
     // slide start 
     function slideStart(eq){
         $(".slide-box").animate({left:itemWidth * eq + 'px'},  200);
-        currentIdx=eq;
+        currentIdx=eq;        
     };
     // console.log(currentIdx);
 
@@ -31,14 +31,16 @@ $(function(){
         if($(this).hasClass("next")){
             if(currentIdx === itemLength-1){
                 // return false;
-                $(".slide-box").append(cloneFirst);
+                $(cloneFirst).insertAfter(lastItem   );
             }else{
                 slideStart(currentIdx -1);
             }
 
         }else{
             if(currentIdx === 0){
-                // return false;                
+                return false;   
+                // $(".slide-box").append(cloneLast);
+
 
             }else{
                 slideStart(currentIdx +1);
