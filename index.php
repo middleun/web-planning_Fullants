5c877a4493a382432e6f797f7515fdf909f10478
middleun <eunallaco@gmail.com>
2021-04-07 18:10:05 +0900

    210407_banner section 태그 추가

--- index.php
+++ index.php
@@ -1,171 +1,189 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Fullants</title>
-   <meta name="keywords" content="식물, 반려식물, 식물판매, 식물관리">
-   <meta name="title" content="식물과 함께하는 생활을 위해, Fullants">
-   <meta name="subject" content="식물 판매">
-   <meta name="description" content="반려식물의 선택과 구매, 관리까지. 식물과 함께하는 생활, Fullants가 책임지겠습니다.">
-   <meta name="author" content="eunallaco@gmail.com, 이중은">
-   <meta name="robots" content="index,follow">
-   <meta name="copyright" content="copyrights 2021 LEEJUNGEUN.">
-   <meta property="og:type" content="website">
-   <meta property="og:url" content="https://middleun.dothome.co.kr/fullants">
-   <meta property="og:title" content="식물과 함께하는 생활을 위해, Fullants">
-   <meta property="og:description" content="반려식물의 선택과 구매, 관리까지. 식물과 함께하는 생활, Fullants가 책임지겠습니다.">
-   <meta property="og:image" content="http://middleun.dothome.co.kr/fullants/img/top_logo.png">
-   <meta property="og:image:width" content="1200">
-   <meta property="og:image:height" content="630">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Fullants</title>
+        <!-- seo -->
+        <meta name="keywords" content="식물, 반려식물, 식물판매, 식물관리, 식물추천">
+        <meta name="title" content="식물과 함께하는 생활을 위해, Fullants">
+        <meta name="subject" content="식물 판매">
+        <meta name="description" content="반려식물의 선택과 구매, 관리까지. 식물과 함께하는 생활, Fullants가 책임지겠습니다.">
+        <meta name="author" content="eunallaco@gmail.com, 이중은">
+        <meta name="robots" content="index,follow">
+        <meta name="copyright" content="copyrights 2021 LEEJUNGEUN.">
+        
+        <!-- open graph -->
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="https://middleun.dothome.co.kr/fullants">
+        <meta property="og:title" content="식물과 함께하는 생활을 위해, Fullants">
+        <meta property="og:description" content="반려식물의 선택과 구매, 관리까지. 식물과 함께하는 생활, Fullants가 책임지겠습니다.">
+        <meta property="og:image" content="http://middleun.dothome.co.kr/fullants/img/top_logo.png">
+        
+        <!-- favicon link -->
+        <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
 
-   <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
+        <!-- reset style css -->
+        <link rel="stylesheet" href="css/reset.css">
 
-   <!-- reset style css -->
-   <link rel="stylesheet" href="css/reset.css">
+        <!-- style css -->
+        <link rel="stylesheet" href="css/style.css">
 
-   <!-- style css -->
-   <link rel="stylesheet" href="css/style.css">
+        <!-- media css -->
+        <link rel="stylesheet" href="css/media.css">
+        
 
-   <!-- media css -->
-   <link rel="stylesheet" href="css/media.css">
-   
-
-   <!-- font awesome cdn link -->
-   <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
-   
-</head>
+        <!-- font awesome cdn link -->
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
+    
+    </head>
 <body>
 
    <div class="wrap">
-      <header id="header">
-         <div class="center">
-            <div class="top-logo">
-               <img src="./img/top_logo.png" alt="Fullants 로고 이미지">
-            </div>
-            <div class="top-nav">
-               <ul>
-                  <li><a href="#">상품보기</a></li>
-                  <li><a href="#">식물관리법</a></li>
-                  <li><a href="#">고객센터</a></li>
-                  <li><a href="#">자사소개</a></li>
-               </ul>
-            </div>
-            <div class="top-icon">
-               <ul>
-                  <li><a href="#"><i class="fas fa-user"></i></a></li>
-                  <li><a href="#"><i class="fas fa-shopping-cart"></i></a></li>
-               </ul>
-            </div>
-
-         </div>
-         <!-- end of center of header -->
-      </header>
-      <!-- end of header -->
-      <section id="bannerSec" class="section">
-         <div class="banner-overlay"></div>
-         <div class="banner-grid">
-            <div class="banner-tag item">
+        <!--header  -->
+        <header id="header">
+            <div class="center">
+                <div class="top-logo">
+                <img src="./img/top_logo.png" alt="Fullants 로고 이미지">
+                </div>
+                <div class="top-nav">
+                <ul>
+                    <li><a href="#">상품보기</a></li>
+                    <li><a href="#">식물관리법</a></li>
+                    <li><a href="#">고객센터</a></li>
+                    <li><a href="#">자사소개</a></li>
+                </ul>
+                </div>
+                <div class="top-icon">
+                <ul>
+                    <li><a href="#"><i class="fas fa-user"></i></a></li>
+                    <li><a href="#"><i class="fas fa-shopping-cart"></i></a></li>
+                </ul>
+                </div>
 
             </div>
-            <a class="banner-btn button"></a>
-         </div>
-      </section>
-      <!-- end of banner section -->
+            <!-- end of center of header -->
+        </header>
+        <!-- end of header -->
+        <section id="bannerSec" class="section">
+            <div class="banner-overlay"></div>
+            <div class="banner-grid">
+                <div class="banner-tag">
+                </div>
+                <div class="banner-tag">
+                    <h3>&#35;그늘에서잘자라는</h3>
+                </div>
+                <div class="banner-tag">                    
+                </div>
+                <div class="banner-tag">                    
+                </div>
+                <div class="banner-tag">
+                    <h3>&#35;미세먼지제거</h3>
+                </div>
+                <div class="banner-tag">
+                    <h3>&#35;전자파차단</h3>
+                </div>
+                <div class="banner-tag">
+                    <h3>&#35;잎이넓은</h3>
+                </div>
+                <div class="banner-tag">
+                    <a href="#" class="banner-btn button" title="식물 추천 바로가기 버튼">식물추천 바로가기</a>
+                </div>
+                <div class="banner-tag">
+                    
+                </div>
 
-      <section id="sloganSec" class="section">
-         <div class="center">
-            <div class="slogan-tit">
-               <h1>Full of Plants Anywhere</h1>
-               <h2>어디서나 식물과 함께하는 생활을 위해</h2>
+
+                
             </div>
-         </div>
-      </section>
-      <!-- end of slogan section -->
+        </section>
+        <!-- end of banner section -->
+
+        <section id="sloganSec" class="section">
+            <div class="center">
+                <div class="slogan-tit">
+                <h1>Full of Plants Anywhere</h1>
+                <h2>어디서나 식물과 함께하는 생활을 위해</h2>
+                </div>
+            </div>
+        </section>
+        <!-- end of slogan section -->
 
 
-      <section id="categorySec" class="section">
-         <div class="category-grid">
-            <div class="category-item plants">
-               <img src="./img/category_img_1.png" alt="식물 상품 대표이미지">
-               <a href="#" class="category-btn button">식물구매</a>
+        <section id="categorySec" class="section">
+            <div class="category-grid">
+                <div class="category-item plants">
+                <img src="./img/category_img_1.png" alt="식물 상품 대표이미지">
+                <a href="#" class="category-btn button" title="식물 구매 바로가기 버튼">식물구매</a>
+                </div>
+                <div class="category-item pots">
+                <img src="./img/category_img_2.png" alt="화분 상품 대표이미지">
+                <a href="#" class="category-btn button" title="화분 구매 바로가기 버튼">화분구매</a>
+                </div>
+                <div class="category-item tools">
+                <img src="./img/category_img_3.png" alt="가드닝도구 상품 대표이미지">
+                <a href="#" class="category-btn button" title="가드닝도구 구매 바로가기 버튼">가드닝도구구매</a>
+                </div>
             </div>
-            <div class="category-item pots">
-               <img src="./img/category_img_2.png" alt="화분 상품 대표이미지">
-               <a href="#" class="category-btn button">화분구매</a>
-            </div>
-            <div class="category-item tools">
-               <img src="./img/category_img_3.png" alt="가드닝도구 상품 대표이미지">
-               <a href="#" class="category-btn button">가드닝도구구매</a>
-
-            </div>
-         </div>
-      </section>
+        </section>
       <!-- end of category section -->
 
 
-      <section id="slideSec" class="section">
-         <div class="center">
-            <div class="slide-tit title">
-               <h1>sns Review</h1>
+        <section id="slideSec" class="section">
+            <div class="center">
+                <div class="slide-tit title">
+                <h1>sns Review</h1>
+                </div>
+                <!-- end of review title -->
+                <div class="slide-wrap">
+                <div class="slide-con">
+                    <div class="slide-box clear">
+                        <div class="slide-item first">
+                            <img src="./img/slide_img_1.png" alt="sns 후기 이미지">
+                        </div>
+                        <div class="slide-item">
+                            <img src="./img/slide_img_1.png" alt="sns 후기 이미지">
+                        </div>
+                        <div class="slide-item">
+                            <img src="./img/slide_img_1.png" alt="sns 후기 이미지">
+                        </div>
+                        <div class="slide-item">
+                            <img src="./img/slide_img_1.png" alt="sns 후기 이미지">
+                        </div>
+                        <div class="slide-item">
+                            <img src="./img/slide_img_1.png" alt="sns 후기 이미지">
+                        </div>
+                        <div class="slide-item">
+                            <img src="./img/slide_img_1.png" alt="sns 후기 이미지">
+                        </div>
+                        
+                    </div>
+                    <!-- end of slide img box -->
+                </div>
+                <!-- end of slide container -->
+                <div class="slide-btn">
+                    <button type="button" class="prev"><i class="fas fa-chevron-left"></i></button>
+                    <button type="button" class="next"><i class="fas fa-chevron-right"></i></button>
+                </div>
+                <!-- end of slide button -->
+                </div>
+                <!-- end of slide wrap -->
             </div>
-            <!-- end of review title -->
-            <div class="slide-wrap">
-               <div class="slide-con">
-                  <div class="slide-box clear">
-                     <div class="slide-item first">
-                        <img src="./img/slide_img_1.png" alt="sns 후기 이미지">
-                     </div>
-                     <div class="slide-item">
-                        <img src="./img/slide_img_1.png" alt="sns 후기 이미지">
-                     </div>
-                     <div class="slide-item">
-                        <img src="./img/slide_img_1.png" alt="sns 후기 이미지">
-                     </div>
-                     <div class="slide-item">
-                        <img src="./img/slide_img_1.png" alt="sns 후기 이미지">
-                     </div>
-                     <div class="slide-item">
-                        <img src="./img/slide_img_1.png" alt="sns 후기 이미지">
-                     </div>
-                     <div class="slide-item">
-                        <img src="./img/slide_img_1.png" alt="sns 후기 이미지">
-                     </div>
-                     
-                  </div>
-                  <!-- end of slide img box -->
-               </div>
-               <!-- end of slide container -->
-               <div class="slide-btn">
-                  <button type="button" class="prev"><i class="fas fa-chevron-left"></i></button>
-                  <button type="button" class="next"><i class="fas fa-chevron-right"></i></button>
-               </div>
-               <!-- end of slide button -->
-            </div>
-            <!-- end of slide wrap -->
-         </div>
-      </section>
-      <!-- end of slide section -->
+        </section>
+        <!-- end of slide section -->    
+      
+    </div> 
+    <!-- jQuery CDN link -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-      
+    <!-- main js  -->
+    <script src="js/custom.js"></script>
 
-      
-   </div>
-   
-   
-      
+    <!-- slider js -->
+    <script src="js/slider.js"></script>      
    
 </body>
-<!-- jQuery CDN link -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-
-
-<!-- main js  -->
-<script src="js/custom.js"></script>
-
-<!-- slider js -->
-<script src="js/slider.js"></script>
 
 
 
